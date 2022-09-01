51987549dbb1814d5fe8ac7dfdc43267b031c84d
SuhakMV <sugamish@gmail.com>
2022-09-01 19:32:16 +0300

    01091932

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+gallery.addEventListener('click', onlmageClick);
